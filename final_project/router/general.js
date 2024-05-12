653f7c7b70faf190413a23f0e4087efdddc3b3d5
Ruhul <ruhul@appnap.io>
2024-05-12 16:15:44 +0600

    feat: implement get route for book details based on isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,7 +17,13 @@ public_users.get("/", function (req, res) {
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn]);
+  }
+  return res.status(400).json({
+    message: `The book with ISBN ${isbn} could not found`,
+  });
 });
 
 // Get book details based on author
